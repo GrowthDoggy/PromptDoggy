1c5fee78811aff785dc781256b4d0eae291f5923
zhenhangtung <zhenhangtung@gmail.com>
2024-08-09 08:04:54 +0800

    feat(auth): create the model, init controllers and related views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # auth routes
+  resources :users, only: [:create]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
